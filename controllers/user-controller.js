baa5ced040bde947dfb26a3985eea294cae6621b
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2022-01-25 13:56:35 -0700

    fixing user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -4,6 +4,10 @@ const userController = {
     // get all users 
     getAllUsers(req, res) {
         Users.find({})
+        // .populate({ 
+        //     path: 'thoughts', select: '-__v'
+        // })
+        // .select('-__v')
             .then(dbUserData => res.json(dbUserData))
             .catch(err => {
                 console.log(err);
